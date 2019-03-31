cd9a3b89ae6b405caca3ca5c9b9dac5e86c71efd
Gustavo Henrique Lima Oliveira <ghlima@Gustavos-MacBook-Pro.local>
2019-03-31 15:32:34 -0300

    frist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+import { Apollo } from 'apollo-angular';
+
+import gql from 'graphql-tag';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'apollo-client-template';
+
+  constructor(
+    private apollo: Apollo
+  ) { 
+    this.allUsers();
+  }
+
+  allUsers(): void {
+    this.apollo.query({
+      query: gql`
+      query {
+        allUsers {
+          id
+          name
+          email
+        }
+      }
+      `
+    }).subscribe(res => console.log('Query: ', res))
+  }
+
+
 }
